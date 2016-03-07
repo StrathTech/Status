af0d12047fe3f78cd59fb997d7b0e7dfc7c88dfa
Adam McGhie <sysadmin@strathtech.co.uk>
2016-03-07 16:06:01 +0000

    add upscuits stuff

---
+++ js/config.js
@@ -0,0 +1,22 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+    'm776921322-c1f88fbe91f08dc886663d5d',
+    'm776921323-03a8fb8c9a22e30991ce4009',
+    'm776945684-a78a964b2d3e79734bbc9e55',
+    'm776945686-b36f041518b1f9f6726710c2',
+    'm776945693-a6d3cd25647af4804e64ed7e',
+    'm776945689-37baa3203a20c2f3129555da',
+    'm776945692-41009ed5dc3ae4fa3568a46e',
+    'm776945683-16189ffb8b57f4ee054ef9d4',
+    'm776921324-21c04241af6c60a770a00d45',
+    'm776921321-23201779f864f9abe0d099c6',
+    'm777046323-89bbc6851745d989ed607be1',
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
